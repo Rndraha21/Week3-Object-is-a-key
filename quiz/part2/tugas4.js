f1fef70a89dbf2d567b964b2f60f374423064a84
Rndraha21 <robinndraha111@gmail.com>
2025-04-29 17:22:26 +0700

    update

---
+++ quiz/part2/tugas4.js
@@ -0,0 +1,29 @@
+/*
+Soal 4
+Diberikan sebuah function checkAB(str) yang menerima satu parameter berupa string. function tersebut mengembalikan nilai true jika di dalam string tersebut terdapat karakter a dan b yang memiliki jarak 3 karakter lain minimal satu kali. Jika tidak ditemukan sama sekali, kembalikan nilai false. Jarak bisa dari a ke b, atau b ke a.
+
+contoh:
+barbarian kenapa bisa true?
+karena di bagian 'barian' terdapat b dipisah 3 karakter ari(totalnya 3) dan ketemu a
+
+Spasi juga dianggap sebagai karakter
+*/
+
+function checkAB(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === "a" && i + 4 < num.length && num[i + 4] === "b") {
+      return true;
+    }
+    if (num[i] == "b" && i + 4 < num.length && num[i + 4] === "a") {
+      return true;
+    }
+  }
+  return false;
+}
+
+// TEST CASES
+console.log(checkAB("lane borrowed")); // true
+console.log(checkAB("i am sick")); // false
+console.log(checkAB("you are boring")); // true
+console.log(checkAB("barbarian")); // true
+console.log(checkAB("bacon and meat")); // false
